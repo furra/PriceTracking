16bfb8eba2253d5e5c486942c8f97f3b84311164
Felipe Urra <furra@lemontech.cl>
2015-09-10 00:01:27 -0300

    Adjusting pipeline

--- scraper/price_tracking/pipelines.py
+++ scraper/price_tracking/pipelines.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from unidecode import unidecode
+from datetime import datetime
+from pymongo import MongoClient
+import re, hashlib
 
 # Define your item pipelines here
 #
@@ -7,5 +11,81 @@
 
 
 class FalabellaTvPipeline(object):
-    def process_item(self, item, spider):
-        return item
+  client = MongoClient('127.0.0.1', 27017)
+  db = client.price_tracking
+  tv_collection = db.televisions
+
+  def process_item(self, item, spider):
+    attributes = {
+      'brand': item['attributes']['brand'].upper(),
+      'technology': unidecode(item['attributes']['technology'].upper()).strip(),
+      'resolution': unidecode(item['attributes']['resolution'].upper()).strip(),
+      'smart_tv': unidecode(item['attributes']['smart_tv']).lower() == 'si',
+      '3d': unidecode(item['attributes']['3d']).lower() == 'si'
+    }
+    store_info = {
+      item['store_code']: {
+        'url': item['url']
+      }
+    }
+    match = re.match(r'\d+[.,]?\d*', item['attributes']['screen_size'])
+    attributes['screen_size'] = float(match.group().replace(',','.')) if match else 0.0
+
+    #normalize attributes
+
+    code_seed = "%s|%s|%s|%s|%s|%s"%(attributes['3d'],
+                                     attributes['brand'],
+                                     attributes['resolution'],
+                                     attributes['screen_size'],
+                                     attributes['smart_tv'],
+                                     attributes['technology'])
+    code = hashlib.md5(code_seed).hexdigest()
+
+    tv = self.tv_collection.find_one({'code':code})
+    #check if code exists
+    if tv:
+      #add new price history if its different
+
+      result = self.tv_collection.update({'code':code},
+                                      {'$set': {
+                                        'store_info.'+item['store_code']: {
+                                          'url': item['url']
+                                        }
+                                      }
+                                    })
+
+      #add new price if last is different
+      last_price = tv['price_history'][item['store_code']][-1]
+      if last_price['price'] != item['price']:
+        result = self.tv_collection.update({'code':code},
+                                        {'$push': {
+                                          'price_history.'+item['store_code']: {
+                                            'date': datetime.now(),
+                                            'price': item['price'],
+                                            'internet_price': item['internet_price']
+                                          }
+                                        }
+                                      })
+        print "Item updated"
+    else:
+      #create object
+      tv_item = {}
+      tv_item['code'] = code
+      tv_item['store_info'] = store_info
+      tv_item['attributes'] = attributes
+
+      tv_item['availability'] = int(item['stock']) > 0
+
+      tv_item['price_history'] = {
+        item['store_code']: [
+          {
+            'date': datetime.now(),
+            'price': item['price'],
+            'internet_price': item['internet_price']
+          }
+        ]
+      }
+      print "Inserting item"
+      self.tv_collection.insert(tv_item)
+
+    return item
--- scraper/price_tracking/spiders/falabella_tv_spider.py
+++ scraper/price_tracking/spiders/falabella_tv_spider.py
@@ -1,6 +1,9 @@
-import scrapy
-import requests
-import jso
+# encoding: utf-8
+import scrapy, requests
+import json, re
+
+from price_tracking.items import TvItem
+
 class FalabellaTvSpider(scrapy.Spider):
   name = "falabella_tv"
   allowed_domains = ["falabella.com"]
@@ -11,14 +14,17 @@ class FalabellaTvSpider(scrapy.Spider):
   def parse(self, response):
     base_url = "http://www.falabella.com"
     tv_links = response.xpath('//div[@class="cajaLP4x"]//div[@class="quickView"]/a/@href').extract()
-    print "%s link elements read." % tv_link    for link in tv_links:
+    print "%s link elements read." % tv_links
+
+    for link in tv_links:
       tv_link = base_url + link
-      print "Scraping %s"%tv_link
+      print "Scrapng %s"%tv_link
       yield scrapy.Request(tv_link, callback=self.parse_tv_info)
-      exit
 
     print "Advancing to next page"
-    next_page = response.xpath('//div[@id="bul-flecha-derecha"]/a/@href').extract(    if next_page:
+    next_page = response.xpath('//div[@id="bul-flecha-derecha"]/a/@href').extract()
+
+    if next_page:
       print "Parsing next page"
       yield scrapy.Request(base_url + next_page[0], callback=self.parse)
     else:
@@ -36,16 +42,20 @@ class FalabellaTvSpider(scrapy.Spider):
       tv_object = json.loads(req.text)
       if tv_object:
         tv_object = tv_object[0]
-        tv_item = {
-          'price': tv_object['NORMAL'],
-          'internet_price': tv_object['INTERNET'],
-          'brand': tv_object['brand'],
-        }
-        stock = tv_object['stockLevel'].split('=')
+        tv_item = TvItem()
 
-        tv_item['stock'] =  stock[1].replace('}','') if stock else ''
+        tv_item['url'] = response.url
+        tv_item['store_code'] = 'falabella'
+        tv_item['internal_code'] = tv_id
+        tv_item['price'] = tv_object['NORMAL']
+        tv_item['internet_price'] = tv_object['INTERNET']
+
+        match = re.match(r'(\d+)=(\d+)', tv_object['stockLevel'])
+        tv_item['stock'] =  match.group(2) if match else '0'
+
         base_info = response.xpath('//table[@id="tablaFichaT"]')
         tv_item['attributes'] = {}
+        tv_item['attributes']['brand'] = tv_object['brand']
 
         tech = base_info.xpath(u'//th[text()="Tecnología"]/following-sibling::*/text()').extract()
         tv_item['attributes']['technology'] = tech[0] if tech else ''
@@ -56,13 +66,13 @@ class FalabellaTvSpider(scrapy.Spider):
         screen_size = base_info.xpath(u'//th[text()="Tamaño de pantalla"]/following-sibling::*/text()').extract()
         tv_item['attributes']['screen_size'] = screen_size[0] if screen_size else ''
 
-        smart_tb = base_info.xpath(u'//th[text()="Smart TV"]/following-sibling::*/text()').extract()
-        tv_item['attributes']['smart_tb'] = smart_tb[0] if smart_tb else ''
+        smart_tv = base_info.xpath(u'//th[text()="Smart TV"]/following-sibling::*/text()').extract()
+        tv_item['attributes']['smart_tv'] = smart_tv[0] if smart_tv else ''
 
         three_d = base_info.xpath(u'//th[text()="3D"]/following-sibling::*/text()').extract()
-        tv_item['attributes']['three_d'] = three_d[0] if three_d else ''
+        tv_item['attributes']['3d'] = three_d[0] if three_d else ''
 
-        yield TvItem(data = tv_item)
+        return tv_item
 
       else:
         print "Tv data in request is empty\n%s"%(base_url%tv_id)
